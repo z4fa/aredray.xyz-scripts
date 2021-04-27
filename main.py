bb5b247ef2707370e76e2925b6ac5515f39db0e3
z4fa <78763346+z4fa@users.noreply.github.com>
2021-04-27 22:30:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 import time
 
-driver1 = webdriver.Chrome("C:\\Users\\Artur\\Desktop\\chromedriver.exe")
-driver2 = webdriver.Chrome("C:\\Users\\Artur\\Desktop\\chromedriver.exe")
-driver3 = webdriver.Chrome("C:\\Users\\Artur\\Desktop\\chromedriver.exe")
-driver4 = webdriver.Chrome("C:\\Users\\Artur\\Desktop\\chromedriver.exe")
-driver5 = webdriver.Chrome("C:\\Users\\Artur\\Desktop\\chromedriver.exe")
+driver1 = webdriver.Chrome("C:\\Users\\Administrator\\Desktop\\chromedriver.exe")
+driver2 = webdriver.Chrome("C:\\Users\\Administrator\\Desktop\\chromedriver.exe")
+driver3 = webdriver.Chrome("C:\\Users\\Administrator\\Desktop\\chromedriver.exe")
+driver4 = webdriver.Chrome("C:\\Users\\Administratorr\\Desktop\\chromedriver.exe")
+driver5 = webdriver.Chrome("C:\\Users\\Administrator\\Desktop\\chromedriver.exe")
 x = 0
 
+url = "https://www.aredray.xyz/viewtopic.php?f=example&t=example"
+
 while True:
-    driver1.get("https://www.aredray.xyz/viewtopic.php?f=4&t=162")
-    driver2.get("https://www.aredray.xyz/viewtopic.php?f=4&t=162")
-    driver3.get("https://www.aredray.xyz/viewtopic.php?f=4&t=162")
-    driver4.get("https://www.aredray.xyz/viewtopic.php?f=4&t=162")
-    driver5.get("https://www.aredray.xyz/viewtopic.php?f=4&t=162")
+    driver1.get(url)
+    driver2.get(url)
+    driver3.get(url)
+    driver4.get(url)
+    driver5.get(url)
     time.sleep(10)
     x += 1
     print(x)
